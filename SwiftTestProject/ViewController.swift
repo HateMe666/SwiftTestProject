fddc3b5a88235c29abbe2d8832525edd338ae565
HateMe666 <nikita.kuzmich.11@gmail.com>
2022-06-06 19:09:54 +0300

    first commit

---
+++ SwiftTestProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftTestProject
+//
+//  Created by Nikita Kuzmich on 6.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
